946c2ce2f9cd9a66a6deb0b20bd3055efe4247c2
crhistian <crhistianbetetanavarro@gmail.com>
2024-08-22 11:37:01 +0200

    fix: change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import sitemap from "@astrojs/sitemap";
 
 const SERVER_PORT = 3000;
 const LOCALHOST = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://github.com/crhistianbeteta/crhistianbeteta.github.io";
+const LIVE_URL = "https://crhistianbeteta.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST;
+console.log("**************", SCRIPT)
 if (isBuild) {
+
   BASE_URL = LIVE_URL;
 }
+console.log("base", BASE_URL)
 
 
 // https://astro.build/config
